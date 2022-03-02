601fc651fe7b8ad52262d5812a0ee769a45cdc9b
narendrabhadineni <narendrabhadineni@gmail.com>
2022-03-02 10:29:34 +0530

    diamond

---
+++ com.diamond/src/com/diamond/TestClass.java
@@ -0,0 +1,32 @@
+package com.diamond;
+
+
+interface First 
+{  
+    default void show() 
+    { 
+        System.out.println("Default First"); 
+    } 
+} 
+interface Second 
+{  
+    default void show() 
+    { 
+        System.out.println("Default Second"); 
+    } 
+}  
+public class TestClass implements First, Second 
+{  
+    public void show() 
+    {  
+        First.super.show(); 
+        Second.super.show(); 
+    } 
+    public static void main(String args[]) 
+    { 
+        TestClass t1 = new TestClass(); 
+        t1.show(); 
+    } 
+}
+
+
