c6c04e3f841e09c0d27e0e123c5d99d26ab11439
narendrabhadineni <100192529+narendrabhadineni@users.noreply.github.com>
2022-03-07 18:51:18 +0530

    Add files via upload

---
+++ final_assessment.java
@@ -0,0 +1,14 @@
+
+package assessment;
+
+import assessment.virtual_key.HandleOptions;
+import assessment.virtual_key.MainMenuOptions;
+
+public class final_assessment 
+{
+	public static void main(String[] args) {
+		MainMenuOptions.welcomeScreen();
+		HandleOptions.WelcomeScreen();
+		virtual_key.viewFiles("Phase1/Hello");
+	}
+	}
